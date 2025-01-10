117677ff4ce50b7f956a085a16f5d945aa056a5f
Alex Nair <alexnair@me.com>
2025-01-09 20:16:25 -0700

    Initial Commit

---
+++ ScrumdingerTests/ScrumdingerTests.swift
@@ -0,0 +1,16 @@
+//
+//  ScrumdingerTests.swift
+//  ScrumdingerTests
+//
+//  Created by Alex Nair on 1/9/25.
+//
+
+import Testing
+
+struct ScrumdingerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
